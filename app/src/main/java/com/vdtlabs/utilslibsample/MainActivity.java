f479a0de2e0df25f92ac81f87c828c64416a8849
Vinod Dirishala <22494324+vinoddirishala@users.noreply.github.com>
2019-05-26 19:39:54 +0530

    Sample code added in sample app

--- app/src/main/java/com/vdtlabs/utilslibsample/MainActivity.java
+++ app/src/main/java/com/vdtlabs/utilslibsample/MainActivity.java
@@ -2,13 +2,21 @@ package com.vdtlabs.utilslibsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.vdtlabs.utilslib.ProjectUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    String appInfo = "";
+    TextView appInfofromLib;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        appInfofromLib = (TextView)findViewById(R.id.appInfofromLib);
+        appInfo = ProjectUtils.getAppInfo();
+        appInfofromLib.setText(appInfo.equalsIgnoreCase("") ? "App Info Not Available":appInfo);
     }
 }
